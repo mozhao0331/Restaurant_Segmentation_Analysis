0e380960ff12419766b484dadf2ae214736574c7
Eric Tsai <eric.tsai1208@gmail.com>
2023-05-23 11:20:25 -0700

    clean up code into more helpers

--- src/build_smoothie_king_model.py
+++ src/build_smoothie_king_model.py
@@ -1,21 +1,16 @@
+import os
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-from sklearn.impute import SimpleImputer
+from joblib import dump, load
 from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import train_test_split
 from sklearn.pipeline import make_pipeline
-from sklearn.preprocessing import OneHotEncoder, OrdinalEncoder, StandardScaler, LabelEncoder
+from sklearn.preprocessing import OrdinalEncoder, StandardScaler, LabelEncoder
 from sklearn.compose import make_column_transformer
-from sklearn.multiclass import OneVsOneClassifier, OneVsRestClassifier
-from sklearn.model_selection import RandomizedSearchCV, GridSearchCV
-from sklearn.ensemble import RandomForestClassifier, VotingClassifier, StackingClassifier
-from sklearn.svm import SVC
+from sklearn.multiclass import OneVsRestClassifier
+from sklearn.ensemble import RandomForestClassifier, VotingClassifier
 from sklearn.feature_selection import SelectFromModel
-from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay
-from catboost import CatBoostClassifier
-from lightgbm.sklearn import LGBMClassifier
-from xgboost import XGBClassifier
 
 
 DIR = 'data/Smoothie King/'
@@ -28,13 +23,11 @@ def read_data():
     merged = stores.merge(trade_area, left_on="store", right_on="store_num").merge(poi)
     return merged
 
-
 def drop_unused_cols(df):
     drop_cols = ["store_num", "country_code", "store", "longitude", "latitude", "state_name", "cbsa_name", "dma_name"]
     df = df.drop(columns=drop_cols)
     df = df.dropna()
     return df
-
 
 def define_preprocessor(X_train):
     market_levels = [
@@ -56,15 +49,12 @@ def define_preprocessor(X_train):
     ]
     ordinal_features = ["market_size", "store_density"]
     numeric_features = X_train.select_dtypes(include=[np.number]).columns.tolist()
-    ALL_FEATURES = numeric_features + ordinal_features
-
     ordinal_transformer = OrdinalEncoder(categories=[market_levels, density_levels], dtype=int)
     preprocessor = make_column_transformer(
         (StandardScaler(), numeric_features),
         (ordinal_transformer, ordinal_features),
     )
     return preprocessor
-
 
 def build_random_forest_model(X_train, y_train, preprocessor, class_weight):
     pipe_rf = make_pipeline(
@@ -143,6 +133,19 @@ def build_l1_reg_random_forest_ovr(X_train, y_train, preprocessor):
     pipe_lr_rf_ovr.fit(X_train, y_train)
     return pipe_lr_rf_ovr
 
+def build_ensemble_model(classifiers, X_train, y_train):
+    hard_voting_model = VotingClassifier(
+        list(classifiers.items()), voting="hard"
+    )
+    hard_voting_model.fit(X_train, y_train)
+    return hard_voting_model
+
+def print_train_test_scores(classifiers, X_train, y_train, X_test, y_test):
+    for classifier_name, model in classifiers.items():
+        print(f"{classifier_name} train score: {model.score(X_train, y_train)}")
+        print(f"{classifier_name} test score: {model.score(X_test, y_test)}")
+
+
 def main():
     sk_df = read_data()
     sk_df = drop_unused_cols(sk_df)
@@ -164,15 +167,27 @@ def main():
     class_weight = {i: class_weight[label] for i, label in enumerate(le.classes_)}
     preprocessor = define_preprocessor(X_train)
     rf_model = build_random_forest_model(X_train, y_train, preprocessor, class_weight)
-    print(f"Random Forest model train score: {rf_model.score(X_train, y_train)}")
-    print(f"Random Forest model test score: {rf_model.score(X_test, y_test)}")
     l1_reg_rf_model = build_l1_reg_random_forest(X_train, y_train, preprocessor, class_weight)
-    print(f"L1 regularization Random Forest train score: {l1_reg_rf_model.score(X_train, y_train)}")
-    print(f"L1 regularization Random Forest test score: {l1_reg_rf_model.score(X_test, y_test)}")
     l1_reg_rf_ovr_model = build_l1_reg_random_forest_ovr(X_train, y_train, preprocessor)
-    print(f"L1 regularization Random Forest OVR train score: {l1_reg_rf_ovr_model.score(X_train, y_train)}")
-    print(f"L1 regularization Random Forest OVR train score: {l1_reg_rf_ovr_model.score(X_test, y_test)}")
-
+    classifiers = {
+        "Random Forest": rf_model,
+        "L1 Regularization Random Forest": l1_reg_rf_model,
+        "L1 Regularization Random Forest OVR": l1_reg_rf_ovr_model
+    }
+    hard_voting_model = build_ensemble_model(classifiers, X_train, y_train)
+    classifiers["Ensemble"] = hard_voting_model
+    print_train_test_scores(classifiers, X_train, y_train, X_test, y_test)
+    # try:
+    #     dump(rf_model, "model_joblib/rf_model.joblib")
+    #     dump(l1_reg_rf_model, "model_joblib/l1_reg_rf_model.joblib")
+    #     dump(l1_reg_rf_ovr_model, "model_joblib/l1_reg_rf_ovr_model.joblib")
+    #     dump(hard_voting_model, "model_joblib/hard_voting_model.joblib")
+    # except:
+    #     os.mkdir("model_joblib/")
+    #     dump(rf_model, "rf_model.joblib")
+    #     dump(l1_reg_rf_model, "l1_reg_rf_model.joblib")
+    #     dump(l1_reg_rf_ovr_model, "l1_reg_rf_ovr_model.joblib")
+    #     dump(hard_voting_model, "model_joblib/hard_voting_model.joblib")
 
 if __name__ == "__main__":
     main()
