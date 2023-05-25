b3ac2125ae296e2f6db47ff69a9c8f6116bc0c40
Chen Lin <chen.lin.0404@gmail.com>
2023-05-24 23:13:19 -0700

    install selenium to interact with Chrome browser

--- src/cluster_verify.py
+++ src/cluster_verify.py
@@ -3,20 +3,23 @@ import urllib
 import urllib.parse
 import urllib.request
 
+# install selenium to interact with Chrome browser ('pip install selenium')
+from selenium import webdriver
+
 
 def main():
     latitude = 37.42199  # Latitude of place
     longitude = -122.08408 # Longitude of place
     zoom = 16  # Zoom level (1-21)
 
-    map_url = 'https://maps.googleapis.com/maps/api/staticmap?'
+    map_url = 'https://maps.googleapis.com/maps/api/js?'
     center = '{},{}'.format(latitude, longitude)
     map_url += urllib.parse.urlencode({
         'center': center,
         'zoom': zoom,
         'size': '600x300',
         'maptype': 'roadmap',
-        'key': 'YOUR_API_KEY'  # Replace with your Google Maps API key
+        'key': 'AIzaSyDSIEnpgGi6s6cCMrLNSTCglznpu-9g7co'  # Replace with your Google Maps API key
     })
 
     map_file = urllib.request.urlopen(map_url)
