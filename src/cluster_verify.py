4a5fd0eb0da278c4a863a6a57d16c28c9e3c53ce
Chen Lin <chen.lin.0404@gmail.com>
2023-05-24 23:54:19 -0700

    locate place based on latitude and longitude

--- src/cluster_verify.py
+++ src/cluster_verify.py
@@ -11,32 +11,34 @@ from selenium.webdriver.support import expected_conditions as EC
 
 
 def main():
-    latitude = 37.42199  # Latitude of place
-    longitude = -122.08408 # Longitude of place
-    zoom = 16  # Zoom level (1-21)
+    latitude = 30.519353  # Latitude of place
+    longitude = -97.650392 # Longitude of place
+    zoom = 12  # Zoom level (1-21)
+    place_url = f'https://www.google.com/maps/place/{latitude},{longitude}'
 
-    map_url = 'https://maps.googleapis.com/maps/api/js?'
-    center = '{},{}'.format(latitude, longitude)
-    map_url += urllib.parse.urlencode({
-        'center': center,
-        'zoom': zoom,
-        'size': '600x300',
-        'maptype': 'roadmap',
-        'key': 'AIzaSyDSIEnpgGi6s6cCMrLNSTCglznpu-9g7co'  # Replace with your Google Maps API key
-    })
+
+    # map_url = 'https://maps.googleapis.com/maps/api/js?'
+    # center = '{},{}'.format(latitude, longitude)
+    # map_url += urllib.parse.urlencode({
+    #     'center': center,
+    #     'zoom': zoom,
+    #     'size': '600x300',
+    #     'maptype': 'roadmap',
+    #     'key': 'AIzaSyDSIEnpgGi6s6cCMrLNSTCglznpu-9g7co'  # Replace with your Google Maps API key
+    # })
 
     # Download the chrome driver file from https://chromedriver.storage.googleapis.com/index.html
     # Ensure the driver version matches the chrome browser version
     driver = webdriver.Chrome('/usr/local/bin/chromedriver') # Path to chrome driver Unix Executable File
-    driver.get("https://www.google.com/maps")
+    driver.get(place_url)
     driver.set_page_load_timeout(10) # Wait up to 10 seconds for pages to load 
 
     WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, "searchboxinput")))
 
     # search_box = driver.find_element('//*[@id="searchboxinput"]')
-    search_box = driver.find_element(By.ID, "searchboxinput")
-    search_box.send_keys('New York, NY')
-    search_box.submit()
+    # search_box = driver.find_element(By.ID, "searchboxinput")
+    # search_box.send_keys('New York, NY')
+    # search_box.submit()
 
     # driver.execute_script('map.setZoom(14);')
 
