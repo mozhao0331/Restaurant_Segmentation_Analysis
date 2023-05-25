1e6cffc46752e12afa559381059dfa0774cf23dd
Chen Lin <chen.lin.0404@gmail.com>
2023-05-25 00:41:35 -0700

    load map with satellite view

--- src/cluster_verify.py
+++ src/cluster_verify.py
@@ -14,7 +14,7 @@ def main():
     latitude = 30.519353  # Latitude of place
     longitude = -97.650392 # Longitude of place
     zoom = 12  # Zoom level (1-21)
-    place_url = f'https://www.google.com/maps/place/{latitude},{longitude}'
+    place_url = f'https://www.google.com/maps/place/{latitude},{longitude}/data=!3m1!1e3!5m2!1e1!1e4'
 
 
     # map_url = 'https://maps.googleapis.com/maps/api/js?'
@@ -30,19 +30,31 @@ def main():
     # Download the chrome driver file from https://chromedriver.storage.googleapis.com/index.html
     # Ensure the driver version matches the chrome browser version
     driver = webdriver.Chrome('/usr/local/bin/chromedriver') # Path to chrome driver Unix Executable File
+    driver.set_window_size(1540,1240)
     driver.get(place_url)
 
     # Load Google Maps API to define the map object
-    driver.execute_script("""
-    var script = document.createElement('script');  
-    script.src = 'https://maps.googleapis.com/maps/api/js?key=AIzaSyDSIEnpgGi6s6cCMrLNSTCglznpu-9g7co'; 
-    document.head.appendChild(script);  
-    script.onload = function() {
-        map.setMapTypeId(google.maps.MapTypeId.SATELLITE); 
-        map.setZoom(14);
-    }    
-    """)
-                          
+    # driver.execute_script("""
+    # var script = document.createElement('script');  
+    # script.src = 'https://maps.googleapis.com/maps/api/js?key=AIzaSyDhdS3PxDvGx0KXJHVLXsQF5R7LEpCW0K4'; 
+    # document.head.appendChild(script);  
+    # script.onload = function() {
+    #     map.setMapTypeId(google.maps.MapTypeId.SATELLITE); 
+    #     map.setZoom(2);
+    # };
+    # """)        
+
+    # driver.execute_async_script("""
+    # window.map = null;
+    # var script = document.createElement('script');
+    # script.src = 'https://maps.googleapis.com/maps/api/js?key=AIzaSyDSIEnpgGi6s6cCMrLNSTCglznpu-9g7co'; 
+    # document.head.appendChild(script);
+    # script.onload = function() { 
+    #     window.map = google.maps.Map.fromDiv(this);  
+    #     window.map.setMapTypeId(google.maps.MapTypeId.SATELLITE);
+    #     window.map.setZoom(2);
+    # };
+    # """)              
     driver.set_page_load_timeout(10) # Wait up to 10 seconds for pages to load 
 
     WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, "searchboxinput")))
