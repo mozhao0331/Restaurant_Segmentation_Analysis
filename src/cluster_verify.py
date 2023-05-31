163faa5598a2cb19d32a46dca9071d790811918b
Chen Lin <chen.lin.0404@gmail.com>
2023-05-30 21:40:05 -0700

    change script to open local html file, no longer the google map page

--- src/cluster_verify.py
+++ src/cluster_verify.py
@@ -1,4 +1,5 @@
 
+import os
 import urllib
 import urllib.parse
 import urllib.request
@@ -11,31 +12,16 @@ from selenium.webdriver.support import expected_conditions as EC
 
 
 def main():
-    latitude = 30.519353  # Latitude of place
-    longitude = -97.650392 # Longitude of place
-    zoom = 12  # Zoom level (1-21)
-    place_url = f'https://www.google.com/maps/place/{latitude},{longitude}/data=!3m1!1e3!5m2!1e1!1e4'
-
+    cwd = os.getcwd()
+    html_file = os.path.join(cwd, 'src/cluster_verify.html')
+ 
     # Download the chrome driver file from https://chromedriver.storage.googleapis.com/index.html
     # Ensure the driver version matches the chrome browser version
     driver = webdriver.Chrome('/usr/local/bin/chromedriver') # Path to chrome driver Unix Executable File
     driver.set_window_size(1540,1240)
-    driver.get(place_url)
-
-    # Load Google Maps API to define the map object
-    # driver.execute_script("""
-    # var script = document.createElement('script');  
-    # script.src = 'https://maps.googleapis.com/maps/api/js?key=AIzaSyDhdS3PxDvGx0KXJHVLXsQF5R7LEpCW0K4'; 
-    # document.head.appendChild(script);  
-    # script.onload = function() {
-    #     map.setMapTypeId(google.maps.MapTypeId.SATELLITE); 
-    #     map.setZoom(2);
-    # };
-    # """)        
+    driver.get('file://' + html_file)
 
     driver.set_page_load_timeout(10) # Wait up to 10 seconds for pages to load 
-
-    WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, "searchboxinput")))
 
     input("Press enter to quit")
 
